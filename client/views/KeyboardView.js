6d662791027a033436ffe5d76fa080fe15e225cc
Heidi Kumar <heidi.kristine.hansen@gmail.com>
2015-10-08 17:25:36 -0700

    Change keyboard display color for full keyboard.

--- client/views/KeyboardView.js
+++ client/views/KeyboardView.js
@@ -10,14 +10,55 @@ var KeyboardView = Backbone.View.extend({
       'e': '#path45','f': '#path71','g': '#path73','h': '#path75','i': '#path55',
       'j': '#path77','k': '#path79','l': '#path81','m': '#path97','n': '#path95',
       'o': '#path57','p': '#path59','q': '#path41','r': '#path47','s': '#path67',
-      't': '#path49','u': '#path53','v': '#path65','w': '#path43','x': '#path89',
+      't': '#path53','u': '#path195','v': '#path65','w': '#path43','x': '#path89',
       'y': '#path51','z': '#path87','A': '#path65','B': '#path93','C': '#path91',
       'D': '#path69','E': '#path45','F': '#path71','G': '#path73','H': '#path75',
       'I': '#path55','J': '#path77','K': '#path79','L': '#path81','M': '#path97',
       'N': '#path95','O': '#path57','P': '#path59','Q': '#path41','R': '#path47',
-      'S': '#path67','T': '#path49','U': '#path53','V': '#path65','W': '#path43',
-      'X': '#path89','Y': '#path51','Z': '#path87'
-    }
+      'S': '#path67','T': '#path49','U': '#path53','V': '#path195','W': '#path43',
+      'X': '#path89','Y': '#path51','Z': '#path87','~': '#path11','`': '#path11',
+      '1': '#path13','!': '#path13','2': '#path15','@': '#path15','3': '#path17',
+      '#': '#path17','4': '#path19','$': '#path19','5': '#path21','%': '#path21',
+      '6': '#path23','^': '#path23','7': '#path25','&': '#path25','8': '#path27',
+      '*': '#path27','9': '#path29','(': '#path29','0': '#path31',')': '#path31',
+      '-': '#path33','_': '#path33','=': '#path35','+': '#path35','\\': '#path37',
+      '|': '#path37','{': '#path61','[': '#path61','}': '#path63',']': '#path63',
+      ';': '#path83',':': '#path83','\'': '#path85', '\"': '#path85', ',': '#path99',
+      '<': '#path99','.': '#path101','>': '#path101', '.':'#path101','/': '#path103',
+      '?': '#path103'
+    },
+
+    //Paths for top row of keyboard
+    keyboardTopRow: [
+      "#path105", "#path107", "#path109", "#path111", "#path113", "#path115",
+      "#path117", "#path119", "#path119", "#path121", "#path123", "#path125",
+      "#path127", "#path129", "#path131"
+    ],
+
+    //Paths for the right side extra keys
+    keyboardArrowsAndNumberBlock: [
+      "#path131", "#path133", "#path135", "#path137", "#path139", "#path141",
+      "#path143", "#path145", "#path147", "#path149", "#path151", "#path153",
+      "#path155", "#path157", "#path159", "#path161", "#path163", "#path165",
+      "#path167", "#path169", "#path171", "#path173", "#path175", "#path177",
+      "#path179", "#path181", "#path183", "#path191", "#path193", "#path373",
+      "#path407", "#path417"
+    ],
+
+    //Paths for the side of the main keyboard
+    keyboardSideKeys: [
+      "#path189", "#path567", "#path565", "#path559", "#path561", "#path563",
+      "#path553", "#path39", "#path187", "#path865", "#path199", "#path197",
+      "#path551"],
+
+    //Paths for legend colors
+    legendClearOut: ["#path781", "#path783", "#path785", "#path817", "#path799",
+        "#path801", "#path803", "#path805", "#path819"],
+
+    //Paths for legend text
+    textToClear : ["#g805", "#g809", "#g813", "#g787", "#g791", "#g795",
+        "#g821", "#g825"]
+
   },
 
   initialize: function () {
@@ -33,15 +74,14 @@ var KeyboardView = Backbone.View.extend({
   },
 
   makeKeyboard : function (xml) {
+    var library = that.defaults.keyboardPathLibrary;
+    console.log(library);
+
       document.body
       .appendChild(xml.documentElement)
       .setAttribute("id", "keyboard");
 
-      var toClearOut = ["#path781", "#path783", "#path785", "#path817", "#path799",
-        "#path801", "#path803", "#path805", "#path819"];
-      //Paths for legend text
-      var textToClear = ["#g805", "#g809", "#g813", "#g787", "#g791", "#g795",
-        "#g821", "#g825"];
+
 
       var fill = "#97c5d5";
 
@@ -58,20 +98,41 @@ var KeyboardView = Backbone.View.extend({
       });
 
       //clearing out the colors from the legend
-      for (var i=0; i<toClearOut.length; i++) {
-        d3.select('#keyboard')
-          .selectAll("g")
-          .select(toClearOut[i])
-          .style("fill", "#eeeeee");
-      };
+      that.modifyKeyboardColor(that.defaults.legendClearOut, "#eeeeee");
 
       //clearing out the text from the legend
-      for (var j=0; j<textToClear.length; j++) {
+      for (var j=0; j<that.defaults.textToClear.length; j++) {
         d3.select('#keyboard')
           .selectAll("g")
-          .selectAll(textToClear[j])
+          .selectAll(that.defaults.textToClear[j])
           .remove();
       };
+
+      //turning MAIN keyboard keys to slate grey
+      for (var key in library) {
+        d3.select('#keyboard')
+          .selectAll("g")
+          .selectAll(library[key])
+          .style("fill", "#A8A8A8");
+      };
+
+      //turning TOP ROW keyboard keys to light grey
+      that.modifyKeyboardColor(that.defaults.keyboardTopRow, "#AEBECD");
+
+      //turning ARROWS AND NUMBERS BLOCK keyboard keys to other color
+      that.modifyKeyboardColor(that.defaults.keyboardArrowsAndNumberBlock, "#CACAD4");
+
+      //turning KEYS ON SIDE OF KEYBOARD to other color
+      that.modifyKeyboardColor(that.defaults.keyboardSideKeys, "#CEC8C8");
+  },
+
+  modifyKeyboardColor: function (array, hexColor) {
+    for (var i=0; i<array.length; i++) {
+        d3.select('#keyboard')
+          .selectAll("g")
+          .selectAll(array[i])
+          .style("fill", hexColor);
+    };
   },
 
   interpretToColor : function (ratio) {
