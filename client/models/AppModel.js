3b7692aec9c25aff05c2e890d0de1d0ab34ce27d
zfreedman <zfreedman92@gmail.com>
2015-10-08 20:30:38 -0700

    Update on Feature_BarGraph_zf - Restylized KeyPressModel and BarGraphView

---
+++ client/models/AppModel.js
@@ -0,0 +1,13 @@
+var AppModel = Backbone.Model.extend({
+
+  /*
+  * Initializes backbone models to be used in views.
+  */
+  initialize: function(params) {
+    this.set('gameDisplay', new GameDisplayModel());
+    this.set('speedTyper', new SpeedTyperModel());
+    // this.set('keyboard', new KeyboardModel());
+    this.set('keyPressModel', new KeyPressModel());
+  }
+
+});
